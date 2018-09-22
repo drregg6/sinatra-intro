7a2c6604cf49879ca00d7461b10b2f13ef00ca30
Dave <drregg6@gmail.com>
2018-09-22 13:24:43 -0400

    dynamically updates background-color upon guess

--- web_guesser.rb
+++ web_guesser.rb
@@ -1,30 +1,41 @@
 require 'sinatra'
 
 SECRET_NUMBER = Random.rand(100)
+msg = ""
+bg_flag = :neutral
 
 def check_guess(num)
     if num == 0
-        "Guess between 1 and 99!"
+        bg_flag = :neutral
+        msg = "Guess between 1 and 99!"
     elsif num - 9 > SECRET_NUMBER
-        "WAY too high!"
+        bg_flag = :wrong
+        msg = "WAY too high!"
     elsif num > SECRET_NUMBER
-        "Too high"
+        bg_flag = :close
+        msg = "Too high"
     elsif num + 9 < SECRET_NUMBER
-        "WAY too low!"
+        bg_flag = :wrong
+        msg = "WAY too low!"
     elsif num < SECRET_NUMBER
-        "Too low"
+        bg_flag = :close
+        msg = "Too low"
     elsif num == SECRET_NUMBER
-        "YOU GOT IT RIGHT!"
+        bg_flag = :correct
+        msg = "YOU GOT IT RIGHT!"
     end
+    return [bg_flag, msg]
 end
 
 get '/' do
     guess = params["guess"].to_i
-    message = check_guess(guess)
+    message = check_guess(guess)[1]
+    bg_flag = check_guess(guess)[0]
     erb :index, :locals => {
         :num => SECRET_NUMBER,
         :message => message,
-        :guess => guess
+        :guess => guess,
+        :bg_flag => bg_flag
     }
 end
 
